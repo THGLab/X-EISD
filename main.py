5dd299d617b763e5f9568c79f668cb880d60189d
Mojtaba <mojtabah@buffalo.edu>
2019-11-25 13:17:14 -0800

    support for single and dual modes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 np.random.seed(90)
 import pandas as pd
 import time
+import os
 
 from parser import read_data
 from meta import meta_data
@@ -341,9 +342,11 @@ def rh_optimization_ensemble(exp_data, bc_data, indices, old_vals=None, popped_s
     return sse, total_score, bc[0]
 
 
-def maximize_score_SAXS(exp_data, bc_data, ens_size, indices, num_structs, old_score_SAXS, old_score_CS, old_score_FRET, old_score_JC,
-                        old_score_NOEs, old_score_PREs, old_score_RDCs, old_score_RH, bc_SAXS_vals, bc_shift_vals,
-                        bc_FRET_val, bc_alpha_vals, bc_dist_vals_NOE, bc_dist_vals_PRE, bc_RDC_vals, bc_RH_val):
+def maximize_score_SAXS(exp_data, bc_data, ens_size, indices, num_structs, flags,
+                        old_score_SAXS, old_score_CS, old_score_FRET, old_score_JC,
+                        old_score_NOEs, old_score_PREs, old_score_RDCs, old_score_RH,
+                        bc_SAXS_vals, bc_shift_vals, bc_FRET_val, bc_alpha_vals,
+                        bc_dist_vals_NOE, bc_dist_vals_PRE, bc_RDC_vals, bc_RH_val):
     indices = list(indices)
     old_SAXS_vals = bc_SAXS_vals
     old_shift_vals = bc_shift_vals
@@ -366,44 +369,73 @@ def maximize_score_SAXS(exp_data, bc_data, ens_size, indices, num_structs, old_s
                 struct_found = True
 
         # SAXS
-        restraint_SSE_SAXS, new_score_SAXS, new_SAXS_vals = saxs_optimization_ensemble(exp_data, bc_data, None,
+        if flags['saxs']:
+            restraint_SSE_SAXS, new_score_SAXS, new_SAXS_vals = saxs_optimization_ensemble(exp_data, bc_data, None,
                                                                                        old_SAXS_vals, popped_structure,
                                                                                        new_index)
+        else:
+            restraint_SSE_SAXS, new_score_SAXS, new_SAXS_vals = [0,0,0]
 
         # CS
-        restraint_SSE_CS, new_score_CS, new_shift_vals = cs_optimization_ensemble(exp_data, bc_data, None,
+        if flags['cs']:
+            restraint_SSE_CS, new_score_CS, new_shift_vals = cs_optimization_ensemble(exp_data, bc_data, None,
                                                                                        old_shift_vals, popped_structure,
                                                                                        new_index)
+        else:
+            restraint_SSE_CS, new_score_CS, new_shift_vals = [0,0,0]
+
 
         # FRET
-        restraint_SSE_FRET, new_score_FRET, new_FRET_val = fret_optimization_ensemble(exp_data, bc_data, None,
+        if flags['fret']:
+            restraint_SSE_FRET, new_score_FRET, new_FRET_val = fret_optimization_ensemble(exp_data, bc_data, None,
                                                                                        old_FRET_val, popped_structure,
                                                                                        new_index)
+        else:
+            restraint_SSE_FRET, new_score_FRET, new_FRET_val = [0,0,0]
 
         # JC
-        restraint_SSE_JC, new_score_JC, new_alpha_vals, jcoup_vals = jc_optimization_ensemble(exp_data, bc_data, None,
+        if flags['jc']:
+            restraint_SSE_JC, new_score_JC, new_alpha_vals, jcoup_vals = jc_optimization_ensemble(exp_data, bc_data, None,
                                                                                        old_alpha_vals, popped_structure,
                                                                                        new_index)
+        else:
+            restraint_SSE_JC, new_score_JC, new_alpha_vals, jcoup_vals = [0,0,0,[0]]
 
         # NOE
-        restraint_SSE_NOE, new_score_NOEs, new_dist_vals_NOE = noe_optimization_ensemble(exp_data, bc_data, None,
+        if flags['noe']:
+            restraint_SSE_NOE, new_score_NOEs, new_dist_vals_NOE = noe_optimization_ensemble(exp_data, bc_data, None,
                                                                                        old_dist_vals_NOE, popped_structure,
                                                                                        new_index)
+        else:
+            restraint_SSE_NOE, new_score_NOEs, new_dist_vals_NOE = [0,0,0]
+
 
         # PRE
-        restraint_SSE_PRE, new_score_PREs, new_dist_vals_PRE = pre_optimization_ensemble(exp_data, bc_data, None,
+        if flags['pre']:
+            restraint_SSE_PRE, new_score_PREs, new_dist_vals_PRE = pre_optimization_ensemble(exp_data, bc_data, None,
                                                                                        old_dist_vals_PRE, popped_structure,
                                                                                        new_index)
+        else:
+            restraint_SSE_PRE, new_score_PREs, new_dist_vals_PRE = [0,0,0]
+
 
         # RDC
-        restraint_SSE_RDC, new_score_RDCs, new_RDC_vals = rdc_optimization_ensemble(exp_data, bc_data, None,
+        if flags['rdc']:
+            restraint_SSE_RDC, new_score_RDCs, new_RDC_vals = rdc_optimization_ensemble(exp_data, bc_data, None,
                                                                                        old_RDC_vals, popped_structure,
                                                                                        new_index)
+        else:
+            restraint_SSE_RDC, new_score_RDCs, new_RDC_vals = [0,0,0]
+
 
         # RH
-        restraint_SSE_RH, new_score_RH, new_RH_val = rh_optimization_ensemble(exp_data, bc_data, None,
+        if flags['rh']:
+            restraint_SSE_RH, new_score_RH, new_RH_val = rh_optimization_ensemble(exp_data, bc_data, None,
                                                                                        old_RH_val, popped_structure,
                                                                                        new_index)
+        else:
+            restraint_SSE_RH, new_score_RH, new_RH_val = [0,0,0]
+
 
         # if iterations%1000 == 0:
         #     print("\n*** iteration %i\n"%iterations)
@@ -421,21 +453,21 @@ def maximize_score_SAXS(exp_data, bc_data, ens_size, indices, num_structs, old_s
             #print repr(i+1) + ' ' + repr(new_score_SAXS) + ' ' + repr(old_score_SAXS) + ' ' + repr(indices)
             old_score_SAXS = new_score_SAXS
             old_score_CS = new_score_CS
+            old_score_FRET = new_score_FRET
+            old_score_JC = new_score_JC
+            old_score_NOEs = new_score_NOEs
+            old_score_PREs = new_score_PREs
+            old_score_RDCs = new_score_RDCs
+            old_score_RH = new_score_RH
+
             old_SAXS_vals = new_SAXS_vals
             old_shift_vals = new_shift_vals
-            old_score_FRET = new_score_FRET
             old_FRET_val = new_FRET_val
-            old_score_JC = new_score_JC
             old_alpha_vals = new_alpha_vals
-            old_score_NOEs = new_score_NOEs
             old_dist_vals_NOE = new_dist_vals_NOE
-            old_score_PREs = new_score_PREs
             old_dist_vals_PRE = new_dist_vals_PRE
-            old_score_RDCs = new_score_RDCs
             old_RDC_vals = new_RDC_vals
-            old_score_RH = new_score_RH
             old_RH_val = new_RH_val
-            accepted = accepted + 1
 
             new_SSE_SAXS = restraint_SSE_SAXS
             new_SSE_CS = restraint_SSE_CS
@@ -446,10 +478,33 @@ def maximize_score_SAXS(exp_data, bc_data, ens_size, indices, num_structs, old_s
             new_SSE_RDC = restraint_SSE_RDC
             new_SSE_RH = restraint_SSE_RH
 
+            accepted = accepted + 1
+
     return old_score_SAXS, new_SSE_SAXS, old_score_CS, new_SSE_CS, old_score_FRET, new_SSE_FRET, old_score_JC, new_SSE_JC, old_score_NOEs, new_SSE_NOE, old_score_PREs, new_SSE_PRE, old_score_RDCs, new_SSE_RDC, old_score_RH, new_SSE_RH, accepted, indices, jcoup_vals
 
 
-def main(exp_data, bc_data, ens_size=100, epochs=250, output_file=None, verbose=False):
+def modes(mode):
+    flags = {'saxs': False, 'cs': False, 'fret':False, 'jc': False,
+             'noe': False,  'pre': False, 'rdc':False, 'rh': False
+             }
+
+    if mode is 'all':
+        return {flag:True for flag in flags}
+
+    elif type(mode) is list:
+        for flag in mode:
+            flags[flag] = True
+        return flags
+
+    elif type(mode) is str:
+        flags[mode] = True
+        return flags
+
+    else:
+        raise ValueError("The mode in the main function is not recognized.")
+
+
+def main(exp_data, bc_data, ens_size=100, epochs=250, mode='all', output_file=None, verbose=False):
     """"""
 
     #  get size
@@ -459,45 +514,71 @@ def main(exp_data, bc_data, ens_size=100, epochs=250, output_file=None, verbose=
     # time
     t0 = time.time()
 
+    flags = modes(mode)
+
     textfile = open(output_file, 'w')
     for it in range(epochs):
         # random selection with replacement
         indices = np.random.randint(0, pool_size, ens_size)   # shape: (100,)
 
         # SAXS optimization on ensemble
-        sse_saxs, total_score_saxs, bc_saxs = saxs_optimization_ensemble(exp_data, bc_data, indices)
+        if flags['saxs']:
+            sse_saxs, total_score_saxs, bc_saxs = saxs_optimization_ensemble(exp_data, bc_data, indices)
+        else:
+            sse_saxs, total_score_saxs, bc_saxs = [None, 0, None]
 
         # CS optimization on ensemble
-        sse_cs, total_score_cs, bc_cs = cs_optimization_ensemble(exp_data, bc_data, indices)
+        if flags['cs']:
+            sse_cs, total_score_cs, bc_cs = cs_optimization_ensemble(exp_data, bc_data, indices)
+        else:
+            sse_cs, total_score_cs, bc_cs = [None, 0, None]
 
         # FRET optimization on ensemble
-        sse_fret, total_score_fret, bc_fret  = fret_optimization_ensemble(exp_data, bc_data, indices)
+        if flags['fret']:
+            sse_fret, total_score_fret, bc_fret = fret_optimization_ensemble(exp_data, bc_data, indices)
+        else:
+            sse_fret, total_score_fret, bc_fret = [None, 0, None]
 
         # JC optimization on ensemble
-        sse_jc, total_score_jc, bc_alpha_vals_jc, _  = jc_optimization_ensemble(exp_data, bc_data, indices)
+        if flags['jc']:
+            sse_jc, total_score_jc, bc_alpha_vals_jc, _  = jc_optimization_ensemble(exp_data, bc_data, indices)
+        else:
+            sse_jc, total_score_jc, bc_alpha_vals_jc, _ = [None, 0, None, None]
 
         # NOEs optimization on ensemble
-        sse_noe, total_score_noe, bc_dist_vals_noe  = noe_optimization_ensemble(exp_data, bc_data, indices)
+        if flags['noe']:
+            sse_noe, total_score_noe, bc_dist_vals_noe = noe_optimization_ensemble(exp_data, bc_data, indices)
+        else:
+            sse_noe, total_score_noe, bc_dist_vals_noe = [None, 0, None]
 
         # PREs optimization on ensemble
-        sse_pre, total_score_pre, bc_pre  = pre_optimization_ensemble(exp_data, bc_data, indices)
+        if flags['pre']:
+            sse_pre, total_score_pre, bc_pre  = pre_optimization_ensemble(exp_data, bc_data, indices)
+        else:
+            sse_pre, total_score_pre, bc_pre = [None, 0, None]
 
         # RDCs optimization on ensemble
-        sse_rdc, total_score_rdc, bc_rdc  = rdc_optimization_ensemble(exp_data, bc_data, indices)
+        if flags['rdc']:
+            sse_rdc, total_score_rdc, bc_rdc  = rdc_optimization_ensemble(exp_data, bc_data, indices)
+        else:
+            sse_rdc, total_score_rdc, bc_rdc = [None, 0, None]
 
         # RH optimization on ensemble
-        sse_rh, total_score_rh, bc_rh  = rh_optimization_ensemble(exp_data, bc_data, indices)
+        if flags['rh']:
+            sse_rh, total_score_rh, bc_rh  = rh_optimization_ensemble(exp_data, bc_data, indices)
+        else:
+            sse_rh, total_score_rh, bc_rh = [None, 0, None]
 
         # Fianl maximization
         max_score_SAXS, opt_SSE_SAXS, max_score_CS, opt_SSE_CS, max_score_FRET, opt_SSE_FRET, max_score_JC, opt_SSE_JC, \
         max_score_NOEs, opt_SSE_NOE, max_score_PREs, opt_SSE_PRE, max_score_RDCs, opt_SSE_RDC, max_score_RH, opt_SSE_RH, \
-        accepted, new_indices, best_jcoups = maximize_score_SAXS(exp_data, bc_data, ens_size, indices, pool_size, total_score_saxs,
-                                                                 total_score_cs, total_score_fret, total_score_jc,
+        accepted, new_indices, best_jcoups = maximize_score_SAXS(exp_data, bc_data, ens_size, indices, pool_size, flags,
+                                                                 total_score_saxs, total_score_cs, total_score_fret,
+                                                                 total_score_jc,
                                                                  total_score_noe, total_score_pre, total_score_rdc,
-                                                                 total_score_rh, bc_saxs, bc_cs,
-                                                                 bc_fret,
-                                                                 bc_alpha_vals_jc, bc_dist_vals_noe, bc_pre,
-                                                                 bc_rdc, bc_rh)
+                                                                 total_score_rh,
+                                                                 bc_saxs, bc_cs, bc_fret, bc_alpha_vals_jc,
+                                                                 bc_dist_vals_noe, bc_pre, bc_rdc, bc_rh)
         s = repr(it) + ' ' + repr(accepted) + ' ' + repr(max_score_SAXS) + ' ' + repr(
             max_score_CS) + ' ' + repr(max_score_FRET) + ' ' + repr(max_score_JC) + ' ' + repr(
             max_score_NOEs) + ' ' + repr(max_score_PREs) + ' ' + repr(max_score_RDCs) + ' ' + repr(
@@ -514,9 +595,49 @@ def main(exp_data, bc_data, ens_size=100, epochs=250, output_file=None, verbose=
     textfile.close()
 
 
-# read data
-filenames = meta_data()
-exp_data = read_data(filenames[0], mode='exp')
-bc_data = read_data(filenames[1], mode='trades_uf')
-main(exp_data, bc_data, epochs=250, output_file='drksh3_unfolded_trades.txt', verbose=True)
-#Todo: fix for other modes
+def make_paris():
+    all = ['saxs', 'cs', 'fret', 'jc', 'noe', 'pre', 'rdc', 'rh']
+    pairs=[]
+    for i in range(len(all)):
+        for j in range(i+1, len(all)):
+            pairs.append([all[i],all[j]])
+    return pairs
+
+
+if __name__ == '__main__':
+
+    filenames = meta_data()
+    exp_data = read_data(filenames[0], mode='exp')
+    bc_data = read_data(filenames[1], mode='trades_uf')
+
+    run_mode = 'dual'
+
+    # all
+    if run_mode == 'all':
+        abs_output = "unfolded/all"
+        if not os.path.exists(abs_output):
+            os.makedirs(abs_output)
+
+        output_file = os.path.join(abs_output, 'drksh3_unfolded_trades_all.txt')
+        main(exp_data, bc_data, epochs=250, mode='all', output_file=output_file, verbose=True)
+
+    # dual mode
+    elif run_mode == 'dual':
+        abs_output = "unfolded/dual_mode"
+        if not os.path.exists(abs_output):
+            os.makedirs(abs_output)
+
+        pairs = make_paris()
+        for pair in pairs:
+            output_file = os.path.join(abs_output, "drksh3_unfolded_trades_%s_%s.txt"%(pair[0], pair[1]))
+            main(exp_data, bc_data, epochs=1000, mode=pair, output_file=output_file, verbose=True)
+
+    # single mode
+    elif run_mode == 'single':
+        abs_output = "unfolded/single_mode"
+        if not os.path.exists(abs_output):
+            os.makedirs(abs_output)
+
+        for mode in ['saxs', 'cs', 'fret', 'jc', 'noe', 'pre', 'rdc', 'rh']:
+            output_file = os.path.join(abs_output, "drksh3_unfolded_trades_%s.txt"%mode)
+            main(exp_data, bc_data, epochs=1000, mode=mode, output_file=output_file, verbose=True)
