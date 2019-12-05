fedaf9a73ad62313216d32da4b3444905f26f694
Mojtaba <mojtabah@buffalo.edu>
2019-12-05 10:44:45 -0800

    save files in csv dataframe format

--- eisd/optimizer.py
+++ eisd/optimizer.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import os
 import time
 
 from eisd.utils import modes
@@ -146,7 +148,7 @@ def maximize_score_SAXS(exp_data, bc_data, ens_size, indices, num_structs, flags
     return old_score_SAXS, new_SSE_SAXS, old_score_CS, new_SSE_CS, old_score_FRET, new_SSE_FRET, old_score_JC, new_SSE_JC, old_score_NOEs, new_SSE_NOE, old_score_PREs, new_SSE_PRE, old_score_RDCs, new_SSE_RDC, old_score_RH, new_SSE_RH, accepted, indices, jcoup_vals
 
 
-def main(exp_data, bc_data, ens_size=100, epochs=250, mode='all', output_file=None, verbose=False):
+def main(exp_data, bc_data, ens_size=100, epochs=250, mode='all', optimization=True, output_dir=None, verbose=False):
     """"""
 
     #  get size
@@ -158,7 +160,10 @@ def main(exp_data, bc_data, ens_size=100, epochs=250, mode='all', output_file=No
 
     flags = modes(mode)
 
-    textfile = open(output_file, 'w')
+    final_results = []
+    final_indices = []
+    final_best_jcoups = []
+
     for it in range(epochs):
         # random selection with replacement
         indices = np.random.randint(0, pool_size, ens_size)   # shape: (100,)
@@ -167,72 +172,103 @@ def main(exp_data, bc_data, ens_size=100, epochs=250, mode='all', output_file=No
         if flags['saxs']:
             sse_saxs, total_score_saxs, bc_saxs = saxs_optimization_ensemble(exp_data, bc_data, indices)
         else:
-            sse_saxs, total_score_saxs, bc_saxs = [None, 0, None]
+            sse_saxs, total_score_saxs, bc_saxs = [0, 0, 0]
 
         # CS optimization on ensemble
         if flags['cs']:
             sse_cs, total_score_cs, bc_cs = cs_optimization_ensemble(exp_data, bc_data, indices)
         else:
-            sse_cs, total_score_cs, bc_cs = [None, 0, None]
+            sse_cs, total_score_cs, bc_cs = [0, 0, 0]
 
         # FRET optimization on ensemble
         if flags['fret']:
             sse_fret, total_score_fret, bc_fret = fret_optimization_ensemble(exp_data, bc_data, indices)
         else:
-            sse_fret, total_score_fret, bc_fret = [None, 0, None]
+            sse_fret, total_score_fret, bc_fret = [0, 0, 0]
 
         # JC optimization on ensemble
         if flags['jc']:
-            sse_jc, total_score_jc, bc_alpha_vals_jc, _  = jc_optimization_ensemble(exp_data, bc_data, indices)
+            sse_jc, total_score_jc, bc_alpha_vals_jc, best_jcoups  = jc_optimization_ensemble(exp_data, bc_data, indices)
         else:
-            sse_jc, total_score_jc, bc_alpha_vals_jc, _ = [None, 0, None, None]
+            sse_jc, total_score_jc, bc_alpha_vals_jc, best_jcoups = [0, 0, 0, [0]]
 
         # NOEs optimization on ensemble
         if flags['noe']:
             sse_noe, total_score_noe, bc_dist_vals_noe = noe_optimization_ensemble(exp_data, bc_data, indices)
         else:
-            sse_noe, total_score_noe, bc_dist_vals_noe = [None, 0, None]
+            sse_noe, total_score_noe, bc_dist_vals_noe = [0, 0, 0]
 
         # PREs optimization on ensemble
         if flags['pre']:
             sse_pre, total_score_pre, bc_pre  = pre_optimization_ensemble(exp_data, bc_data, indices)
         else:
-            sse_pre, total_score_pre, bc_pre = [None, 0, None]
+            sse_pre, total_score_pre, bc_pre = [0, 0, 0]
 
         # RDCs optimization on ensemble
         if flags['rdc']:
             sse_rdc, total_score_rdc, bc_rdc  = rdc_optimization_ensemble(exp_data, bc_data, indices)
         else:
-            sse_rdc, total_score_rdc, bc_rdc = [None, 0, None]
+            sse_rdc, total_score_rdc, bc_rdc = [0, 0, 0]
 
         # RH optimization on ensemble
         if flags['rh']:
             sse_rh, total_score_rh, bc_rh  = rh_optimization_ensemble(exp_data, bc_data, indices)
         else:
-            sse_rh, total_score_rh, bc_rh = [None, 0, None]
+            sse_rh, total_score_rh, bc_rh = [0, 0, 0]
 
-        # Fianl maximization
-        max_score_SAXS, opt_SSE_SAXS, max_score_CS, opt_SSE_CS, max_score_FRET, opt_SSE_FRET, max_score_JC, opt_SSE_JC, \
-        max_score_NOEs, opt_SSE_NOE, max_score_PREs, opt_SSE_PRE, max_score_RDCs, opt_SSE_RDC, max_score_RH, opt_SSE_RH, \
-        accepted, new_indices, best_jcoups = maximize_score_SAXS(exp_data, bc_data, ens_size, indices, pool_size, flags,
-                                                                 total_score_saxs, total_score_cs, total_score_fret,
-                                                                 total_score_jc,
-                                                                 total_score_noe, total_score_pre, total_score_rdc,
-                                                                 total_score_rh,
-                                                                 bc_saxs, bc_cs, bc_fret, bc_alpha_vals_jc,
-                                                                 bc_dist_vals_noe, bc_pre, bc_rdc, bc_rh)
-        s = repr(it) + ' ' + repr(accepted) + ' ' + repr(max_score_SAXS) + ' ' + repr(
-            max_score_CS) + ' ' + repr(max_score_FRET) + ' ' + repr(max_score_JC) + ' ' + repr(
-            max_score_NOEs) + ' ' + repr(max_score_PREs) + ' ' + repr(max_score_RDCs) + ' ' + repr(
-            max_score_RH) + ' ' + repr((opt_SSE_SAXS / 37.0) ** 0.5) + ' ' + repr(
-            (opt_SSE_CS / 262.0) ** 0.5) + ' ' + repr(opt_SSE_FRET ** 0.5) + ' ' + repr(
-            (opt_SSE_JC / 47.0) ** 0.5) + ' ' + repr((opt_SSE_NOE / 93.0) ** 0.5) + ' ' + repr(
-            (opt_SSE_PRE / 68.0) ** 0.5) + ' ' + repr((opt_SSE_RDC / 28.0) ** 0.5) + ' ' + repr(
-            opt_SSE_RH ** 0.5) + ' ' + repr(new_indices) + ' ' + repr(best_jcoups) + '\n'
-        textfile.write(s)
-        textfile.flush()
+        # optimization
+        if optimization:
+            max_score_SAXS, opt_SSE_SAXS, max_score_CS, opt_SSE_CS, max_score_FRET, opt_SSE_FRET, max_score_JC, opt_SSE_JC, \
+            max_score_NOEs, opt_SSE_NOE, max_score_PREs, opt_SSE_PRE, max_score_RDCs, opt_SSE_RDC, max_score_RH, opt_SSE_RH, \
+            accepted, new_indices, best_jcoups = maximize_score_SAXS(exp_data, bc_data, ens_size, indices, pool_size, flags,
+                                                                     total_score_saxs, total_score_cs, total_score_fret,
+                                                                     total_score_jc,
+                                                                     total_score_noe, total_score_pre, total_score_rdc,
+                                                                     total_score_rh,
+                                                                     bc_saxs, bc_cs, bc_fret, bc_alpha_vals_jc,
+                                                                     bc_dist_vals_noe, bc_pre, bc_rdc, bc_rh)
+        else:
+            accepted = 0
+            new_indices = indices
+
+            max_score_SAXS = total_score_saxs
+            max_score_CS = total_score_cs
+            max_score_FRET = total_score_fret
+            max_score_JC = total_score_jc
+            max_score_NOEs = total_score_noe
+            max_score_PREs = total_score_pre
+            max_score_RDCs = total_score_rdc
+            max_score_RH = total_score_rh
+
+            opt_SSE_SAXS = sse_saxs
+            opt_SSE_CS = sse_cs
+            opt_SSE_FRET = sse_fret
+            opt_SSE_JC = sse_jc
+            opt_SSE_NOE = sse_noe
+            opt_SSE_PRE = sse_pre
+            opt_SSE_RDC = sse_rdc
+            opt_SSE_RH = sse_rh
+
+        s = [it, accepted,max_score_SAXS, max_score_CS,max_score_FRET,max_score_JC,
+                          max_score_NOEs, max_score_PREs, max_score_RDCs, max_score_RH,
+                (opt_SSE_SAXS / 37.0) ** 0.5,
+                (opt_SSE_CS / 262.0) ** 0.5,
+                opt_SSE_FRET ** 0.5,
+                (opt_SSE_JC / 47.0) ** 0.5,
+                (opt_SSE_NOE / 93.0) ** 0.5,
+                (opt_SSE_PRE / 68.0) ** 0.5,
+                (opt_SSE_RDC / 28.0) ** 0.5,
+                opt_SSE_RH ** 0.5
+             ]
+        final_results.append(s)
+        final_indices.append(new_indices)
+        final_best_jcoups.append(best_jcoups)
 
         if verbose: print("\n### iteration: %i  (elapsed time: %f seconds)"%(it+1, time.time()-t0))
 
-    textfile.close()
+    pd.DataFrame(final_results).to_csv(os.path.join(output_dir, 'results.csv'), index=False, header=False)
+    pd.DataFrame(final_indices).to_csv(os.path.join(output_dir, 'indices.csv'), index=False, header=False)
+    if flags['jc']:
+        pd.DataFrame(final_best_jcoups).to_csv(os.path.join(output_dir, 'best_jcoups.csv'), index=False, header=False)
+
 
