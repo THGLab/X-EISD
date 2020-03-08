5dda5752612b319b7cd75893dbe8317f044d13af
Mojtaba <mojtabah@buffalo.edu>
2020-03-07 23:34:45 -0800

    adopt recent changes

--- sample_script.py
+++ sample_script.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     # parameters
     data_path = "../eisd-pkg" # path to experimental data and structure pools (james' compilation)
-    structure = 'trades_uf'  # ['trades', 'trades_uf', 'mixed', 'ensemble']
+    structure = 'ensemble'  # ['trades', 'trades_uf', 'mixed', 'ensemble']
     # run_mode: How do you want to run the eisd optimizer?
     #   - all: optimize on all experimental observables together
     #   - single: optimize on individual experimental observable
@@ -28,12 +28,13 @@ if __name__ == '__main__':
 
     # run_mode: all
     if run_mode == 'all':
-        abs_output = "newrun/unfolded/all_unoptimized"
+        # abs_output = "local/newrun_2020/%s/all_unoptimized/saxsexp0.1"%structure
+        abs_output = "local/newrun_2020/metroMC/%s/all_optimized/beta.025"%structure
         if not os.path.exists(abs_output):
             os.makedirs(abs_output)
 
         # main(exp_data, bc_data, epochs=1000, mode='all', optimization=True, output_dir=abs_output, verbose=False)
-        main(exp_data, bc_data, epochs=1000, mode='all', optimization=False, output_dir=abs_output, verbose=False)
+        main(exp_data, bc_data, epochs=1000, mode='all', beta=0.025, output_dir=abs_output, verbose=False)
 
     # run_mode: dual
     elif run_mode == 'dual':
@@ -41,7 +42,7 @@ if __name__ == '__main__':
         # pairs = [['saxs', 'jc'],['cs', 'jc'], ['fret', 'jc'],['jc', 'noe'],['jc', 'pre'],['jc', 'rdc'],['jc', 'rh']]
         pairs = [['saxs', 'noe'], ['cs', 'noe'], ['cs', 'pre'], ['cs', 'rdc'],['fret', 'noe'], ['noe', 'pre'], ['noe', 'rdc']]
         for pair in pairs:
-            abs_output = "temp/unfolded/dual_mode/%s_%s"%(pair[0], pair[1])
+            abs_output = "local/newrun_2020/unfolded/dual_mode/%s_%s"%(pair[0], pair[1])
             if not os.path.exists(abs_output):
                 os.makedirs(abs_output)
             main(exp_data, bc_data, epochs=1000, mode=pair, output_dir=abs_output, verbose=False)
@@ -49,10 +50,10 @@ if __name__ == '__main__':
     # run_mode: single
     elif run_mode == 'single':
         # single_modes = ['saxs', 'cs', 'fret', 'jc', 'noe', 'pre', 'rdc', 'rh']
-        single_modes = ['jc']
+        single_modes = ['noe', 'pre', 'rdc', 'rh']
         for mode in single_modes:
-            abs_output = "newrun/unfolded/single_mode/%s"%mode
+            abs_output = "local/newrun_2020/metroMC/%s/single_mode/beta.01/%s"%(structure, mode)
             if not os.path.exists(abs_output):
                 os.makedirs(abs_output)
 
-            main(exp_data, bc_data, epochs=1000, mode=mode, output_dir=abs_output, verbose=True)
+            main(exp_data, bc_data, epochs=1000, mode=mode, beta=0.1, output_dir=abs_output, verbose=True)
