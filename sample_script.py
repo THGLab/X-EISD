8b81370463aa3450730de2605df97f7170278ae7
Mojtaba <mojtabah@buffalo.edu>
2020-03-20 16:04:09 -0700

    local runs

--- sample_script.py
+++ sample_script.py
@@ -20,13 +20,15 @@ if __name__ == '__main__':
     #   - all: optimize on all experimental observables together
     #   - single: optimize on individual experimental observable
     #   - dual: optimize on pairs of experimental observables
-    run_mode = 'single'
-    opt_type = 'mc'
+    # run_mode = 'dual'
+    opt_type = 'max' # 'max', 'mc', None
     beta = 0.1
     # in the new_prop_error directory the following properties are affected: CS, FRET, PRE, SAXS
 
-    # run_mode = 'pre_indices'
-    pre_indices_path = ''
+    run_mode = 'pre_indices'
+    pre_indices_mode = 'dual'  # 'dual' or 'signle'
+    # pre_indices_path = 'local/newrun_2020/new_prop_errors2/positive_mc/%s/single_mode/opt_%s/'%(structure,str(opt_type))
+    pre_indices_path = 'local/newrun_2020/new_prop_errors2/positive_mc/%s/dual_mode/opt_%s/'%(structure,str(opt_type))
 
     # read files
     filenames = meta_data(data_path)
@@ -36,9 +38,9 @@ if __name__ == '__main__':
     # run_mode: all
     if run_mode == 'all':
         if opt_type == 'mc':
-            abs_output = "local/newrun_2020/new_prop_errors2/%s/all_mode/opt_%s_b%s"%(structure,str(opt_type), str(beta))
+            abs_output = "local/newrun_2020/new_prop_errors2/positive_mc/%s/all_mode/opt_%s_b%s"%(structure,str(opt_type), str(beta))
         else:
-            abs_output = "local/newrun_2020/new_prop_errors2/%s/all_mode/opt_%s"%(structure,str(opt_type))
+            abs_output = "local/newrun_2020/new_prop_errors2/positive_mc/%s/all_mode/opt_%s"%(structure,str(opt_type))
 
         if not os.path.exists(abs_output):
             os.makedirs(abs_output)
@@ -50,12 +52,12 @@ if __name__ == '__main__':
     elif run_mode == 'dual':
         # pairs = make_pairs()
         # pairs = [['saxs', 'jc'],['cs', 'jc'], ['fret', 'jc'],['jc', 'noe'],['jc', 'pre'],['jc', 'rdc'],['jc', 'rh']]
-        pairs = [['saxs', 'noe'], ['cs', 'noe'], ['cs', 'pre'], ['cs', 'rdc'],['fret', 'noe'], ['noe', 'pre'], ['noe', 'rdc']]
+        pairs = [['fret', 'noe'], ['fret', 'pre'], ['fret', 'rdc'], ['fret', 'rh']]
         for pair in pairs:
             if opt_type == 'mc':
-                abs_output = "local/newrun_2020/new_prop_errors2/%s/dual_mode/opt_%s_b%s/%s_%s"%(structure, str(opt_type), str(beta) ,pair[0], pair[1])
+                abs_output = "local/newrun_2020/new_prop_errors2/positive_mc/%s/dual_mode/opt_%s_b%s/%s_%s"%(structure, str(opt_type), str(beta) ,pair[0], pair[1])
             else:
-                abs_output = "local/newrun_2020/new_prop_errors2/%s/dual_mode/opt_%s/%s_%s"%(structure, str(opt_type),pair[0], pair[1])
+                abs_output = "local/newrun_2020/new_prop_errors2/positive_mc/%s/dual_mode/opt_%s/%s_%s"%(structure, str(opt_type),pair[0], pair[1])
 
             if not os.path.exists(abs_output):
                 os.makedirs(abs_output)
@@ -64,12 +66,12 @@ if __name__ == '__main__':
     # run_mode: single
     elif run_mode == 'single':
         # single_modes = ['saxs', 'cs', 'fret', 'jc', 'noe', 'pre', 'rdc', 'rh']
-        single_modes = ['noe', 'pre', 'rdc', 'rh']
+        single_modes = ['saxs', 'cs', 'fret']
         for mode in single_modes:
             if opt_type == 'mc':
-                abs_output = "local/newrun_2020/new_prop_errors2/%s/single_mode/opt_%s_b%s/%s"%(structure, str(opt_type),str(beta),mode)
+                abs_output = "local/newrun_2020/new_prop_errors2/positive_mc/%s/single_mode/opt_%s_b%s/%s"%(structure, str(opt_type),str(beta),mode)
             else:
-                abs_output = "local/newrun_2020/new_prop_errors2/%s/single_mode/opt_%s/%s"%(structure, str(opt_type), mode)
+                abs_output = "local/newrun_2020/new_prop_errors2/positive_mc/%s/single_mode/opt_%s/%s"%(structure, str(opt_type), mode)
 
             if not os.path.exists(abs_output):
                 os.makedirs(abs_output)
@@ -78,5 +80,25 @@ if __name__ == '__main__':
 
     # run_mode: pre_indices; to calculate scores/RMSDs for the remaining unoptimized properties
     elif run_mode == 'pre_indices':
-        pre_indices = pd.read_csv(pre_indices_path).values
-        main(exp_data, bc_data, pre_indices=pre_indices)
+        if pre_indices_mode == 'single':
+            for prop in ['saxs', 'cs', 'jc', 'fret', 'noe', 'pre', 'rh', 'rdc']:
+                ind_path = os.path.join(pre_indices_path, prop, 'indices.csv')
+                pre_indices = pd.read_csv(ind_path, header=None).values
+
+                abs_output = os.path.join(pre_indices_path, prop, 'all_processed')
+                print(abs_output)
+                if not os.path.exists(abs_output):
+                    os.makedirs(abs_output)
+                main(exp_data, bc_data, pre_indices=pre_indices, output_dir=abs_output)
+
+        elif pre_indices_mode == 'dual':
+            pairs = make_pairs()
+            for pair in pairs:
+                ind_path = os.path.join(pre_indices_path, '%s_%s'%(pair[0], pair[1]), 'indices.csv')
+                pre_indices = pd.read_csv(ind_path, header=None).values
+
+                abs_output = os.path.join(pre_indices_path, '%s_%s'%(pair[0], pair[1]), 'all_processed')
+                print(abs_output)
+                if not os.path.exists(abs_output):
+                    os.makedirs(abs_output)
+                main(exp_data, bc_data, pre_indices=pre_indices, output_dir=abs_output)
