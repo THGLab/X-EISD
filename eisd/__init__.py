02bab9151ad48b22f5ffee72c3a5b32fa4ccf307
Mojtaba <mojtabah@buffalo.edu>
2021-03-15 18:06:33 -0700

    tested scoring function

--- eisd/__init__.py
+++ eisd/__init__.py
@@ -1,3 +1,3 @@
 __version__ = "0.1.0"
 
-from scoring_function import ensemble_scorer
+from eisd.scoring_function import ensemble_scorer
--- eisd/scoring_function.py
+++ eisd/scoring_function.py
@@ -6,6 +6,7 @@ from eisd.utils import meta_data
 from eisd.parser import read_data
 from eisd.scorers import *
 
+
 def ensemble_scorer(property, bc, data_path='data', exp_indices=None):
     """
     This is a function to calculate EISD score for a given ensemble of conformers.
@@ -49,8 +50,29 @@ def ensemble_scorer(property, bc, data_path='data', exp_indices=None):
 
     indices = np.arange(len(bc))
 
-    if property == 'cs':
-        sse, score, _ = cs_optimization_ensemble(bc_data, exp_data, indices)
+    if property == 'jc':
+        sse, score, _, _ = jc_optimization_ensemble(exp_data, bc_data, indices)
+
+    elif property == 'cs':
+        sse, score, _ = cs_optimization_ensemble(exp_data, bc_data, indices)
+
+    elif property == 'noe':
+        sse, score, _ = noe_optimization_ensemble(exp_data, bc_data, indices)
+
+    elif property == 'rdc':
+        sse, score, _ = rdc_optimization_ensemble(exp_data, bc_data, indices)
+
+    elif property == 'fret':
+        sse, score, _ = fret_optimization_ensemble(exp_data, bc_data, indices)
+
+    elif property == 'saxs':
+        sse, score, _ = saxs_optimization_ensemble(exp_data, bc_data, indices)
+
+    elif property == 'pre':
+        sse, score, _ = pre_optimization_ensemble(exp_data, bc_data, indices)
+
+    elif property == 'rh':
+        sse, score, _ = rh_optimization_ensemble(exp_data, bc_data, indices)
 
     return score
 
